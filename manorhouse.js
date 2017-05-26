067a9ad91b5a0404342fc22467322e982e09e3a8
ChubbyQuark <chris.balkham1@gmail.com>
2017-05-26 19:58:50 +0100

    Update manorhouse.js

--- manorhouse.js
+++ manorhouse.js
@@ -76,8 +76,8 @@ function onLoad() {
   Viewer = new VRView.Player('#PrettyPics', {
     image: 'images/black.jpg',
     preview: 'images/black.jpg',
-    height: '490px',
-    width: '980px',
+    height: '400px',
+    width: '800px',
     is_stereo: true,
     is_yaw_only: true,
     default_yaw: 180 ,
